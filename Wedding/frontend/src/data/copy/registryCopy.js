2ff6f12d2f1e43b257c11d77249df29a04acf7ba
matthewJ1299 <matthew.j@live.com>
2026-04-16 10:02:47 +0200

    Updated bank

--- Wedding/frontend/src/data/copy/registryCopy.js
+++ Wedding/frontend/src/data/copy/registryCopy.js
@@ -6,6 +6,6 @@ export const REGISTRY_COPY = {
   intro: "Your presence at our wedding is truly the greatest gift we could ever ask for. Sharing this special day with the people we love means more to us than words can express, and we feel incredibly grateful that you will be celebrating this milestone with us. We genuinely do not expect anything more. However, if you would like to give a gift, we are so excited for our honeymoon and would deeply appreciate any contribution toward making those first memories as a married couple even more unforgettable.",
   accountDetailsTitle: 'Account Details',
   bankName: 'FNB',
-  accountNumber: '62577644323',
+  accountNumber: '62453888558',
   branch: '250655'
 };
